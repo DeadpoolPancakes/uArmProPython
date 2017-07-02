e9a5d555c340f214cd87c2e66ed375e44e6aaef2
Jamie Naylor <drnailher@gmail.com>
2017-07-02 10:07:31 +0100

    added gripper defination
    works the same as the pump

--- uArmRobot.py
+++ uArmRobot.py
@@ -99,7 +99,10 @@ class robot:
         self.pumping = state
         cmd = protocol.SET_PUMP.format(int(state))
         self.sendcmd(cmd,True)
-
+    def gripper(self, state):
+        self.gripping = state
+        cmd = protocol.SET_GRIPPER.format(int(state))
+        self.sendcmd(cmd,True)
     def mode(self, modeid):
         # 0= Normal
         # 1= Laser
